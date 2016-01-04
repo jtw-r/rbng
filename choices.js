555add3da5e2c6f367b3b7f78beedc47515cdad6
thejonathanr <jonathan.reiterman@gmail.com>
2016-01-03 21:07:30 -0800

    Fixed bugs

--- choices.js
+++ choices.js
@@ -1,15 +1,15 @@
 //Random Things
-var thingsstr = "Apple, Bannana, Cherry, Farm, Orchard, Bear, Book, Bike, Bat, Window, Computer, Wine Press, Table, Cat, Car, Auto Part, Lamp, Tree, Mailbox, Newspaper, Office Supplie, Jewlry, Blanket, Basket, Pen, Paper, Art Supplie, Door, Doorframe, Deck, Pool, Hottub, Shoe, Pantleg, Fur, Piano, Pie, Sink, Bookcase";
-var thingsdata = thingsstr.split(",");
+var thingstr = "Apple, Bannana, Cherry, Farm, Orchard, Bear, Book, Bike, Bat, Window, Computer, Wine Press, Table, Cat, Car, Auto Part, Lamp, Tree, Mailbox, Newspaper, Office Supplie, Jewlry, Blanket, Basket, Pen, Paper, Art Supplie, Door, Doorframe, Deck, Pool, Hottub, Shoe, Pantleg, Fur, Piano, Pie, Sink, Bookcase";
+var thingdata = thingstr.split(",");
 
 //Begining of the name
 var beginingstr = "A, The";
 var beginingdata = beginingstr.split(",");
 
 //People names
-var namesstr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack";
-var namesdata = namesstr.split(",");
+var namestr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack";
+var namedata = namestr.split(",");
 
 //Ending of the name
-var endingsstr = "Business, Company, Ltd., Co., Foundation, Farm, Store, Inc., Corp.";
-var endingsdata = endingsstr.split(",");
+var endingstr = "Business, Company, Ltd., Co., Foundation, Farm, Store, Inc., Corp.";
+var endingdata = endingstr.split(",");
