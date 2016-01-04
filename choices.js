29f9c908b6c7f57e5e69785262b9dcd8f3e8e49c
thejonathanr <jonathan.reiterman@gmail.com>
2016-01-03 20:01:21 -0800

    Version 1.0.0!

---
+++ choices.js
@@ -0,0 +1,11 @@
+var thingsstr = "Apple, Bannana, Cherry, Farm, Orchard, Bear, Book, Bike, Bat, Window, Computer, Wine Press, Table, Cat, Car, Auto Part, Lamp, Tree, Mailbox, Newspaper, Office Supplie, Jewlry, Blanket, Basket, Pen, Paper, Art Supplie, Door, Doorframe, Deck, Pool, Hottub, Shoe, Pantleg, Fur, Piano, Pie, Sink, Bookcase";
+var thingsdata = thingsstr.split(",");
+
+var beginingstr = "A, The";
+var beginingdata = beginingstr.split(",");
+
+var namesstr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack";
+var namesdata = namesstr.split(",");
+
+var endingsstr = "Business, Company, Ltd., Co., Foundation, Farm, Store";
+var endingsdata = endingsstr.split(",");
