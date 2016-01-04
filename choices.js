695174179d0c4435e5bf76d00f8791267f51a418
thejonathanr <jonathan.reiterman@gmail.com>
2016-01-03 20:54:20 -0800

    Added more to choices

--- choices.js
+++ choices.js
@@ -7,5 +7,5 @@ var beginingdata = beginingstr.split(",");
 var namesstr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack";
 var namesdata = namesstr.split(",");
 
-var endingsstr = "Business, Company, Ltd., Co., Foundation, Farm, Store";
+var endingsstr = "Business, Company, Ltd., Co., Foundation, Farm, Store, Inc., Corp., Asscoiation";
 var endingdata = endingsstr.split(",");
