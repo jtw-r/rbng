1da40b87a04f36a74ddf3a54218ce4dff6bce76b
thejonathanr <jonathan.reiterman@gmail.com>
2016-01-18 17:37:21 -0800

    Version 1.0.3

--- choices.js
+++ choices.js
@@ -1,5 +1,5 @@
 //Random Things
-var thingstr = "Apple, Bannana, Cherry, Farm, Orchard, Bear, Book, Bike, Bat, Window, Computer, Wine Press, Table, Cat, Car, Auto Part, Lamp, Tree, Mailbox, Newspaper, Office Stuff, Jewlry, Blanket, Basket, Pen, Paper, Art Stuff, Door, Doorframe, Deck, Pool, Hottub, Shoe, Pantleg, Fur, Piano, Pie, Sink, Bookcase, Hourglass, Case, Bookend, Pellow, Shopingbag, Catbox, Cat Litter, Dishwasher, Dish, TV, Dust, Snow Matchine, Disco Ball, Boardgame";
+var thingstr = "Apple, Bannana, Cherry, Farm, Orchard, Bear, Book, Bike, Bat, Window, Computer, Wine Press, Table, Cat, Car, Auto Part, Lamp, Tree, Mailbox, Newspaper, Office Stuff, Jewlry, Blanket, Basket, Pen, Paper, Art Stuff, Door, Doorframe, Deck, Pool, Hottub, Shoe, Pantleg, Fur, Piano, Pie, Sink, Bookcase, Hourglass, Case, Bookend, Pellow, Shopingbag, Catbox, Cat Litter, Dishwasher, Dish, TV, Dust, Snow Matchine, Disco Ball, Boardgame, Solid State Drive, Flash Drive, Printer, Lightswitch, Power Strip, Hard Drive, Tree Sap, Kidney Bean, Taco, Salsa, Rice, Furnace, Rain Barrel, Foot Cream, Ice cream, Gift Card, Thank You Card";
 var thingdata = thingstr.split(",");
 
 //Begining of the name
@@ -7,9 +7,9 @@ var beginingstr = "A, The";
 var beginingdata = beginingstr.split(",");
 
 //People names
-var namestr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack, Aaron, Susan, Jill, Thomas, Shapel, Brook, Brooklyn, Fred, Casie, Jacqueline";
+var namestr = "Amie, Amanda, Amy, Bob, Bill, Cathie, Caitlian, Cindy, Dick, Dan, Ellie, Frank, George, Georga, Greg, Hillary, Henry, Ian, Joe, Jon, John, Jerry, Jay, Katie, Katy, Kate, Lary, Lucy, Mary, Margret, Naomi, Perry, Russ, Rob, Suzy, Steve, Tom, Tim, Tia, Wanda, Yonda, Zack, Aaron, Susan, Jill, Thomas, Shapel, Brook, Brooklyn, Fred, Casie, Jacqueline, Ruby";
 var namedata = namestr.split(",");
 
 //Ending of the name
-var endingstr = "Business, Company, Ltd., Co., Foundation, Farm, Store, Inc., Corp., Outlet";
+var endingstr = "Business, Company, Ltd., Co., Foundation, Farm, Store, Inc., Corp., Outlet, Group";
 var endingdata = endingstr.split(",");
